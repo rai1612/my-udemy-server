759542b3cf8b526eeffd762db3e134db38fddbd7
kulungRAI <raia41145@gmail.com>
2023-06-28 01:37:55 +0530

    createHmac fix

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -40,8 +40,9 @@ export const paymentVerification = catchAsyncError(async (req, res, next) => {
   const subscription_id = user.subscription.id;
 
   // creating our signature according to razorpay from the payment id
+  crypto.create;
   const generated_signature = crypto
-    .createHmax("sha256", process.env.RAZORPAY_API_SECRET)
+    .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
     .update(razorpay_payment_id + "|" + subscription_id, "utf-8")
     .digest("hex");
 
